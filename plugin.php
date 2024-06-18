9f7ed7c4caf905d818bd03b5f4f302e94054364b
Roman_T <120475330+RomanTrifanov@users.noreply.github.com>
2024-06-18 17:20:48 +0800

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: API Action - Delete
 Plugin URI: http://www.claytondaley.com/
 Description: Adds a "delete" action to the API. This action requires authentication even if the site is public. This action accepts either a shorturl or keyword passed using the "shorturl" parameter.
-Version: 1.0
+Version: 2.0
 Author: Clayton Daley (derived from API-Action by Ozh)
 Author URI: http://www.claytondaley.com/
 */
@@ -20,15 +20,6 @@ function clayton_api_action_delete() {
 		require_once( YOURLS_INC.'/auth.php' );
 	}
 
-	// Need 'shorturl' parameter
-	if( !isset( $_REQUEST['shorturl'] ) ) {
-		return array(
-			'statusCode' => 400,
-			'simple'     => "Need a 'shorturl' parameter",
-			'message'    => 'error: missing param',
-		);	
-	}
-	
 	$shorturl = $_REQUEST['shorturl'];
 
 	// Is $shorturl a URL (http://sho.rt/abc) or a keyword (abc) ?
