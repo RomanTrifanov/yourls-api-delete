313634443c18604d79e7b9ca3a63295c8d27937b
Roman_T <120475330+RomanTrifanov@users.noreply.github.com>
2024-07-04 11:28:44 +0800

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,18 +1,18 @@
 <?php
 /*
 Plugin Name: API Action - Delete
-Plugin URI: http://www.claytondaley.com/
+Plugin URI: https://github.com/RomanTrifanov/yourls-api-delete/blob/master/plugin.php
 Description: Adds a "delete" action to the API. This action requires authentication even if the site is public. This action accepts either a shorturl or keyword passed using the "shorturl" parameter.
 Version: 2.0
-Author: Clayton Daley (derived from API-Action by Ozh)
-Author URI: http://www.claytondaley.com/
+Author: RomanTrifanov
+Author URI:  https://github.com/RomanTrifanov/
 */
 
 // Define custom action "delete"
-yourls_add_filter( 'api_action_delete', 'clayton_api_action_delete' );
+yourls_add_filter( 'api_action_delete', 'rt_api_action_delete' );
 
 // Actually delete
-function clayton_api_action_delete() {
+function rt_api_action_delete() {
 	// We don't want unauthenticated users deleting links
 	// If YOURLS is in public mode, force authentication anyway
 	if (!yourls_is_private()) {
@@ -20,6 +20,15 @@ function clayton_api_action_delete() {
 		require_once( YOURLS_INC.'/auth.php' );
 	}
 
+	// Need 'shorturl' parameter
+	if( !isset( $_REQUEST['shorturl'] ) ) {
+		return array(
+			'statusCode' => 400,
+			'simple'     => "Need a 'shorturl' parameter",
+			'message'    => 'error: missing param',
+		);	
+	}
+	
 	$shorturl = $_REQUEST['shorturl'];
 
 	// Is $shorturl a URL (http://sho.rt/abc) or a keyword (abc) ?
